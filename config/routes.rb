779513fbc882b8c6b02641559a7091fb9d7be571
James Perry <PjPerry28@gmail.com>
2022-11-21 19:33:57 -0800

    create user and api keys table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/api-keys', to: 'api_keys#create'
+  delete '/api-keys', to: 'api_keys#destroy'
+  get '/api-keys', to: 'api_keys#index'
 end

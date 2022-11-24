ce3872f27cacc55d357c8eb4aa271c383799e3f9
James Perry <PjPerry28@gmail.com>
2022-11-23 19:10:49 -0800

    created all tables with necesary relationships

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -11,7 +11,16 @@ class ApiKeysController < ApplicationController
   end
  
   def create
-    
+    authenticate_with_http_basic do |email, password| 
+      user = User.find_by email: email 
+ 
+      if user&.authenticate(password) 
+        api_key = user.api_keys.create! token: SecureRandom.hex 
+ 
+        render json: api_key, status: :created and return 
+      end 
+      
+     render status: :unauthorized
   end
  
   def destroy
